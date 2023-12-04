25d8e4422781562a137aeaa13e2c0e3d09d4a227
Kaila Btw <javacube1@gmail.com>
2023-12-04 16:05:33 -0500

    Turn in quiz 03

---
+++ quizes/quiz03/main.js
@@ -0,0 +1,23 @@
+function color1() {
+    document.querySelector("#square1").style.backgroundColor = "#F5624D";
+}
+
+function color2() {
+    document.querySelector("#square2").style.backgroundColor = "#CC231E";
+}
+
+function color3() {
+    document.querySelector("#square3").style.backgroundColor = "darkred";
+}
+
+function color4() {
+    document.querySelector("#square4").style.backgroundColor = "#0F8A5F";
+}
+
+function color5() {
+    document.querySelector("#square5").style.backgroundColor = "#235E6F";
+}
+
+function color6() {
+    document.querySelector("#square6").style.backgroundColor = "#34A65F";
+}
